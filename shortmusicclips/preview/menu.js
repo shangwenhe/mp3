5a1ee4d203bd72c6d2b767ae8801bbfb0d549478
shangwenhe <shangwenhe@rongcloud.cn>
2023-12-15 15:18:01 +0800

    添加一下 音频文件

---
+++ shortmusicclips/preview/menu.js
@@ -0,0 +1,842 @@
+[{
+    "Category": "Action",
+    "items": [{
+            "name": "Epic Drums",
+            "Category": "Action"
+        },
+        {
+            "name": "Future Man",
+            "Category": "Action"
+        },
+        {
+            "name": "Resistance",
+            "Category": "Action"
+        },
+        {
+            "name": "The Mission",
+            "Category": "Action"
+        },
+        {
+            "name": "The Report",
+            "Category": "Action"
+        },
+        {
+            "name": "Under Siege",
+            "Category": "Action"
+        },
+    ]
+}, {
+    "Category": "Alternative",
+    "items": [{
+            "name": "Black Orchids",
+            "Category": "Alternative"
+        },
+        {
+            "name": "Darkest Hours",
+            "Category": "Alternative"
+        },
+        {
+            "name": "Daytona Rock",
+            "Category": "Alternative"
+        },
+        {
+            "name": "Dirtbag",
+            "Category": "Alternative"
+        },
+        {
+            "name": "Hot Sauce",
+            "Category": "Alternative"
+        },
+        {
+            "name": "Rapid Frequency",
+            "Category": "Alternative"
+        },
+        {
+            "name": "Seattle Song",
+            "Category": "Alternative"
+        },
+        {
+            "name": "White Snow",
+            "Category": "Alternative"
+        },
+        {
+            "name": "Who Cares",
+            "Category": "Alternative"
+        },
+        {
+            "name": "Wild Girl",
+            "Category": "Alternative"
+        },
+    ]
+}, {
+    "Category": "Chill Out",
+    "items": [{
+            "name": "Afterglow",
+            "Category": "Chill Out"
+        },
+        {
+            "name": "Deep Chill",
+            "Category": "Chill Out"
+        },
+        {
+            "name": "Grace",
+            "Category": "Chill Out"
+        },
+        {
+            "name": "Lost Angels",
+            "Category": "Chill Out"
+        },
+        {
+            "name": "Lost In A Moment",
+            "Category": "Chill Out"
+        },
+        {
+            "name": "Peaceful Now",
+            "Category": "Chill Out"
+        },
+        {
+            "name": "Summer Daze",
+            "Category": "Chill Out"
+        },
+        {
+            "name": "Sunset Dreaming",
+            "Category": "Chill Out"
+        },
+        {
+            "name": "The Same Moon",
+            "Category": "Chill Out"
+        },
+        {
+            "name": "Venus Isle",
+            "Category": "Chill Out"
+        },
+        {
+            "name": "Winners Smile",
+            "Category": "Chill Out"
+        },
+    ]
+}, {
+    "Category": "Classic Rock",
+    "items": [{
+            "name": "Boston Rocks",
+            "Category": "Classic Rock"
+        },
+        {
+            "name": "Fab '65",
+            "Category": "Classic Rock"
+        },
+        {
+            "name": "Lead Balloon",
+            "Category": "Classic Rock"
+        },
+        {
+            "name": "Legacy",
+            "Category": "Classic Rock"
+        },
+        {
+            "name": "Moby Dig",
+            "Category": "Classic Rock"
+        },
+        {
+            "name": "Ready Set Go",
+            "Category": "Classic Rock"
+        },
+        {
+            "name": "Retro Active",
+            "Category": "Classic Rock"
+        },
+        {
+            "name": "Roll On",
+            "Category": "Classic Rock"
+        },
+        {
+            "name": "Sabotage",
+            "Category": "Classic Rock"
+        },
+        {
+            "name": "Walkin' Around",
+            "Category": "Classic Rock"
+        },
+    ]
+}, {
+    "Category": "Country",
+    "items": [{
+            "name": "Beer OClock",
+            "Category": "Country"
+        },
+        {
+            "name": "Country Comfort",
+            "Category": "Country"
+        },
+        {
+            "name": "Highway 25",
+            "Category": "Country"
+        },
+        {
+            "name": "Long Walk Home",
+            "Category": "Country"
+        },
+        {
+            "name": "Miles Away",
+            "Category": "Country"
+        },
+        {
+            "name": "Nashville Boogie",
+            "Category": "Country"
+        },
+        {
+            "name": "Old Dirt Road",
+            "Category": "Country"
+        },
+        {
+            "name": "Swamp Stomp",
+            "Category": "Country"
+        },
+        {
+            "name": "To Your Door",
+            "Category": "Country"
+        },
+    ]
+}, {
+    "Category": "Crime & Suspense",
+    "items": [{
+            "name": "Access Denied",
+            "Category": "Crime & Suspense"
+        },
+        {
+            "name": "All That Remains",
+            "Category": "Crime & Suspense"
+        },
+        {
+            "name": "Buried Secrets",
+            "Category": "Crime & Suspense"
+        },
+        {
+            "name": "Cellar Door",
+            "Category": "Crime & Suspense"
+        },
+        {
+            "name": "Elimination",
+            "Category": "Crime & Suspense"
+        },
+        {
+            "name": "Enemy In Disguise",
+            "Category": "Crime & Suspense"
+        },
+        {
+            "name": "Espionage",
+            "Category": "Crime & Suspense"
+        },
+        {
+            "name": "Fear",
+            "Category": "Crime & Suspense"
+        },
+        {
+            "name": "Playing To Win",
+            "Category": "Crime & Suspense"
+        },
+        {
+            "name": "The Strategy",
+            "Category": "Crime & Suspense"
+        },
+        {
+            "name": "The Vanishing",
+            "Category": "Crime & Suspense"
+        },
+        {
+            "name": "Unseen Stranger",
+            "Category": "Crime & Suspense"
+        },
+        {
+            "name": "Watching Waiting",
+            "Category": "Crime & Suspense"
+        },
+    ]
+}, {
+    "Category": "Dance & Techno",
+    "items": [{
+            "name": "Adrenaline",
+            "Category": "Dance & Techno"
+        },
+        {
+            "name": "Airwaves",
+            "Category": "Dance & Techno"
+        },
+        {
+            "name": "All Mixed Up",
+            "Category": "Dance & Techno"
+        },
+        {
+            "name": "Alpha Beta",
+            "Category": "Dance & Techno"
+        },
+        {
+            "name": "Big Bang",
+            "Category": "Dance & Techno"
+        },
+        {
+            "name": "Chemistry",
+            "Category": "Dance & Techno"
+        },
+        {
+            "name": "Club Kids",
+            "Category": "Dance & Techno"
+        },
+        {
+            "name": "Cosmic Gatherings",
+            "Category": "Dance & Techno"
+        },
+        {
+            "name": "Dr Loco",
+            "Category": "Dance & Techno"
+        },
+        {
+            "name": "Hypersonic",
+            "Category": "Dance & Techno"
+        },
+        {
+            "name": "Intoxicate",
+            "Category": "Dance & Techno"
+        },
+        {
+            "name": "Melt With U",
+            "Category": "Dance & Techno"
+        },
+        {
+            "name": "Neurotica",
+            "Category": "Dance & Techno"
+        },
+        {
+            "name": "Shake It",
+            "Category": "Dance & Techno"
+        },
+        {
+            "name": "Smile",
+            "Category": "Dance & Techno"
+        },
+        {
+            "name": "Voodoo Lounge",
+            "Category": "Dance & Techno"
+        },
+    ]
+}, {
+    "Category": "Fashion",
+    "items": [{
+            "name": "Bluewater",
+            "Category": "Fashion"
+        },
+        {
+            "name": "City Life",
+            "Category": "Fashion"
+        },
+        {
+            "name": "Fashion Life",
+            "Category": "Fashion"
+        },
+        {
+            "name": "Hot Gossip",
+            "Category": "Fashion"
+        },
+        {
+            "name": "Island Party",
+            "Category": "Fashion"
+        },
+        {
+            "name": "Madison Ave",
+            "Category": "Fashion"
+        },
+        {
+            "name": "The High Life",
+            "Category": "Fashion"
+        },
+        {
+            "name": "Top Model",
+            "Category": "Fashion"
+        },
+    ]
+}, {
+    "Category": "Hard Rock",
+    "items": [{
+            "name": "Aggressor",
+            "Category": "Hard Rock"
+        },
+        {
+            "name": "Air Raid",
+            "Category": "Hard Rock"
+        },
+        {
+            "name": "Bulletproof",
+            "Category": "Hard Rock"
+        },
+        {
+            "name": "Dominator",
+            "Category": "Hard Rock"
+        },
+        {
+            "name": "Dragon Fire",
+            "Category": "Hard Rock"
+        },
+        {
+            "name": "Fight To Win",
+            "Category": "Hard Rock"
+        },
+        {
+            "name": "Future World",
+            "Category": "Hard Rock"
+        },
+        {
+            "name": "Groove Machine",
+            "Category": "Hard Rock"
+        },
+        {
+            "name": "Hammerhead",
+            "Category": "Hard Rock"
+        },
+        {
+            "name": "Heroes Return",
+            "Category": "Hard Rock"
+        },
+        {
+            "name": "Hybrid Theory",
+            "Category": "Hard Rock"
+        },
+        {
+            "name": "Immortal",
+            "Category": "Hard Rock"
+        },
+        {
+            "name": "Iron Fist",
+            "Category": "Hard Rock"
+        },
+        {
+            "name": "L.A Groove",
+            "Category": "Hard Rock"
+        },
+        {
+            "name": "Metal Machine",
+            "Category": "Hard Rock"
+        },
+        {
+            "name": "Nocturnal",
+            "Category": "Hard Rock"
+        },
+        {
+            "name": "One Giant Leap",
+            "Category": "Hard Rock"
+        },
+        {
+            "name": "Out With The Boys",
+            "Category": "Hard Rock"
+        },
+        {
+            "name": "Panic Stations",
+            "Category": "Hard Rock"
+        },
+        {
+            "name": "Powerdriver",
+            "Category": "Hard Rock"
+        },
+        {
+            "name": "Powersurge",
+            "Category": "Hard Rock"
+        },
+        {
+            "name": "Rip & Tear",
+            "Category": "Hard Rock"
+        },
+        {
+            "name": "Subhuman",
+            "Category": "Hard Rock"
+        },
+        {
+            "name": "The Fury",
+            "Category": "Hard Rock"
+        },
+        {
+            "name": "The Reaper",
+            "Category": "Hard Rock"
+        },
+        {
+            "name": "Top Fuel",
+            "Category": "Hard Rock"
+        },
+        {
+            "name": "Unholy",
+            "Category": "Hard Rock"
+        },
+        {
+            "name": "Valley Of Kings",
+            "Category": "Hard Rock"
+        },
+        {
+            "name": "War Machine",
+            "Category": "Hard Rock"
+        },
+    ]
+}, {
+    "Category": "Hip-Hop",
+    "items": [{
+            "name": "Bad Girls",
+            "Category": "Hip-Hop"
+        },
+        {
+            "name": "Bounce",
+            "Category": "Hip-Hop"
+        },
+        {
+            "name": "Clap Yo Handz",
+            "Category": "Hip-Hop"
+        },
+        {
+            "name": "Concrete Jungle",
+            "Category": "Hip-Hop"
+        },
+        {
+            "name": "Fly Guy",
+            "Category": "Hip-Hop"
+        },
+        {
+            "name": "GangstaLand",
+            "Category": "Hip-Hop"
+        },
+        {
+            "name": "Get Rich",
+            "Category": "Hip-Hop"
+        },
+        {
+            "name": "Heatwave",
+            "Category": "Hip-Hop"
+        },
+        {
+            "name": "Hey Yo",
+            "Category": "Hip-Hop"
+        },
+        {
+            "name": "How You Feel",
+            "Category": "Hip-Hop"
+        },
+        {
+            "name": "Lowrider",
+            "Category": "Hip-Hop"
+        },
+        {
+            "name": "Money Talks",
+            "Category": "Hip-Hop"
+        },
+        {
+            "name": "Nasty",
+            "Category": "Hip-Hop"
+        },
+        {
+            "name": "Sidewayz",
+            "Category": "Hip-Hop"
+        },
+        {
+            "name": "So Fine",
+            "Category": "Hip-Hop"
+        },
+        {
+            "name": "Throw Down",
+            "Category": "Hip-Hop"
+        },
+        {
+            "name": "Urban Edge",
+            "Category": "Hip-Hop"
+        },
+    ]
+}, {
+    "Category": "Pop",
+    "items": [{
+            "name": "All I Want",
+            "Category": "Pop"
+        },
+        {
+            "name": "Almost Summer",
+            "Category": "Pop"
+        },
+        {
+            "name": "Best Of Me",
+            "Category": "Pop"
+        },
+        {
+            "name": "Born To Win",
+            "Category": "Pop"
+        },
+        {
+            "name": "By Your Side",
+            "Category": "Pop"
+        },
+        {
+            "name": "Dirty Jeans",
+            "Category": "Pop"
+        },
+        {
+            "name": "Electroslam",
+            "Category": "Pop"
+        },
+        {
+            "name": "Falling Away",
+            "Category": "Pop"
+        },
+        {
+            "name": "Forever",
+            "Category": "Pop"
+        },
+        {
+            "name": "Funky Kitchen",
+            "Category": "Pop"
+        },
+        {
+            "name": "In The Zone",
+            "Category": "Pop"
+        },
+        {
+            "name": "In Too Deep",
+            "Category": "Pop"
+        },
+        {
+            "name": "Lift You Up",
+            "Category": "Pop"
+        },
+        {
+            "name": "Live It Up",
+            "Category": "Pop"
+        },
+        {
+            "name": "Pink Garbage",
+            "Category": "Pop"
+        },
+        {
+            "name": "Princess Dreams",
+            "Category": "Pop"
+        },
+        {
+            "name": "Reason To Be",
+            "Category": "Pop"
+        },
+        {
+            "name": "Rock Star",
+            "Category": "Pop"
+        },
+        {
+            "name": "School Days",
+            "Category": "Pop"
+        },
+        {
+            "name": "Secret Dreams",
+            "Category": "Pop"
+        },
+        {
+            "name": "Sports World",
+            "Category": "Pop"
+        },
+        {
+            "name": "Starting Over",
+            "Category": "Pop"
+        },
+        {
+            "name": "Straight Ahead",
+            "Category": "Pop"
+        },
+        {
+            "name": "Teen Dream",
+            "Category": "Pop"
+        },
+        {
+            "name": "The Need For Speed",
+            "Category": "Pop"
+        },
+        {
+            "name": "Unlucky",
+            "Category": "Pop"
+        },
+        {
+            "name": "Winning Edge",
+            "Category": "Pop"
+        },
+        {
+            "name": "Without You",
+            "Category": "Pop"
+        },
+    ]
+}, {
+    "Category": "Punk",
+    "items": [{
+            "name": "Broken Bones",
+            "Category": "Punk"
+        },
+        {
+            "name": "Conformity",
+            "Category": "Punk"
+        },
+        {
+            "name": "No Control",
+            "Category": "Punk"
+        },
+        {
+            "name": "Prankster",
+            "Category": "Punk"
+        },
+        {
+            "name": "Punk Kids",
+            "Category": "Punk"
+        },
+        {
+            "name": "Revolution",
+            "Category": "Punk"
+        },
+        {
+            "name": "Riot Squad",
+            "Category": "Punk"
+        },
+        {
+            "name": "Rip It",
+            "Category": "Punk"
+        },
+        {
+            "name": "Smash It Up",
+            "Category": "Punk"
+        },
+        {
+            "name": "Social Disgrace",
+            "Category": "Punk"
+        },
+        {
+            "name": "Stoked",
+            "Category": "Punk"
+        },
+        {
+            "name": "Underdog",
+            "Category": "Punk"
+        },
+    ]
+}, {
+    "Category": "Romantic",
+    "items": [{
+            "name": "Beautiful",
+            "Category": "Romantic"
+        },
+        {
+            "name": "Love Is",
+            "Category": "Romantic"
+        },
+        {
+            "name": "Love Letters",
+            "Category": "Romantic"
+        },
+        {
+            "name": "Loving You",
+            "Category": "Romantic"
+        },
+        {
+            "name": "Missing Pieces",
+            "Category": "Romantic"
+        },
+        {
+            "name": "Moonlight Dance",
+            "Category": "Romantic"
+        },
+        {
+            "name": "Reflections",
+            "Category": "Romantic"
+        },
+        {
+            "name": "This Time",
+            "Category": "Romantic"
+        },
+    ]
+}, {
+    "Category": "Sadness",
+    "items": [{
+            "name": "Not Forgotten",
+            "Category": "Sadness"
+        },
+        {
+            "name": "When You're Gone",
+            "Category": "Sadness"
+        },
+    ]
+}, {
+    "Category": "Science & Tech",
+    "items": [{
+            "name": "Corporate Vision",
+            "Category": "Science & Tech"
+        },
+        {
+            "name": "Innovation",
+            "Category": "Science & Tech"
+        },
+        {
+            "name": "Prestige Design",
+            "Category": "Science & Tech"
+        },
+    ]
+}, {
+    "Category": "Soundscapes",
+    "items": [{
+        "name": "Ethereal",
+        "Category": "Soundscapes"
+    }, ]
+}, {
+    "Category": "Uplifting",
+    "items": [{
+            "name": "Crowning Glory",
+            "Category": "Uplifting"
+        },
+        {
+            "name": "Knights Return",
+            "Category": "Uplifting"
+        },
+        {
+            "name": "Morning View",
+            "Category": "Uplifting"
+        },
+        {
+            "name": "Relics",
+            "Category": "Uplifting"
+        },
+        {
+            "name": "Slipping Away",
+            "Category": "Uplifting"
+        },
+        {
+            "name": "The Fallen",
+            "Category": "Uplifting"
+        },
+        {
+            "name": "The One",
+            "Category": "Uplifting"
+        },
+        {
+            "name": "Top Of The World",
+            "Category": "Uplifting"
+        },
+    ]
+}, {
+    "Category": "World Music",
+    "items": [{
+            "name": "Calypso",
+            "Category": "World Music"
+        },
+        {
+            "name": "Destination Goa",
+            "Category": "World Music"
+        },
+        {
+            "name": "Indian Summer",
+            "Category": "World Music"
+        },
+        {
+            "name": "Jamaica Girl",
+            "Category": "World Music"
+        },
+        {
+            "name": "Mantra",
+            "Category": "World Music"
+        },
+        {
+            "name": "Wind Of Change",
+            "Category": "World Music"
+        },
+    ]
+}]
